9d8a8ba282e38795dcf25b85615ba098aae8087b
alco-cat23 <artemkurnasenko2010@gmail.com>
2025-04-23 18:28:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# импорт библиотек
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return password
+
+
+
+
+# создание пароля
+letter = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLXCVBNM`,.;"\[]{}!@#$%^&*()_+=-:?><|*/'
+password = ''
+for i in range(12):
+    r = random.choice(letter)
+    password = password+r
+# всё, пароль готов
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
